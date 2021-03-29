fe6bd5fcab9229400cabbfcf33392a7c60ce8d14
Alan Oliveira <alanjhone@gmail.com>
2021-03-29 20:33:54 -0300

    add configuration view id row

---
+++ springboot-service-users/src/main/java/com/springboot/app/users/infra/config/RepositoryConfig.java
@@ -0,0 +1,16 @@
+package com.springboot.app.users.infra.config;
+
+import com.springboot.app.users.domain.entity.Role;
+import com.springboot.app.users.domain.entity.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(User.class, Role.class);
+    }
+}
