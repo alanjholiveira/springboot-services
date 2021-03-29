685b4d5bd8cb61066cdf5e37a757b5f56ffb2c7f
Alan Oliveira <alanjhone@gmail.com>
2021-03-29 08:53:14 -0300

    change name rest query

--- springboot-service-users/src/main/java/com/springboot/app/users/infra/repository/UserRepository.java
+++ springboot-service-users/src/main/java/com/springboot/app/users/infra/repository/UserRepository.java
@@ -3,14 +3,17 @@ package com.springboot.app.users.infra.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.springboot.app.users.domain.entity.User;
 
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-	public User findByUsername(String username);
+	@RestResource(path = "get-username")
+	public User findByUsername(@Param("name") String username);
 	
 	@Query("select u from User u where u.username=?1")
 	public User obterPorUsername(String username);
