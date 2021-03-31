3f23b5f4ffda0370778ea831c43755519f574bd6
Alan Oliveira <alanjhone@gmail.com>
2021-03-30 21:47:40 -0300

    removedio users commons

--- springboot-service-users-commons/src/main/java/com/springboot/app/users/commons/SpringBootServiceUsersCommonsApplication.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.app.users.commons;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class SpringBootServiceUsersCommonsApplication {
-
-}
